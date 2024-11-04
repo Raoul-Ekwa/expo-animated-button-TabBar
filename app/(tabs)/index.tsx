09f299f6962231674f7ec902da725070d008fa32
ekwaRaoul <ekwaraoul@yahoo.com>
2024-11-04 13:42:08 +0100

    Ajout du bouton 'continuez ici avec la redirection vers le menu explore

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, Image, View } from 'react-native';
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native'; // Importez useNavigation
 
 const Page = () => {
+  const navigation = useNavigation(); // Récupérez l'objet de navigation
+
+  const handlePress = () => {
+    navigation.navigate('explore'); // Naviguez vers la route 'Explore'
+  };
+
   return (
-   <View style={styles.container}>
-    <Text>Home Screen</Text>
-   </View>
-  )
-}
+    <View style={styles.container}>
+      <Image
+        source={require('..//../assets/images/logo_santaLucia.png')}
+        style={styles.logoHomePage}
+      />
+      <TouchableOpacity style={styles.buttonContainer} onPress={handlePress}>
+        <Text style={styles.buttonText}>Continuez ici</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default Page;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 100,
+    backgroundColor: '#ff69b4', // code hexadecimal rose de santa lucia 
+  },
+  logoHomePage: {
+    width: 310,
+    height: 200,
+    marginBottom: 50, // espace ajusté 
+  },
+  buttonContainer: {
+    padding: 25,
+    backgroundColor: '#1d3e7b',
+    borderRadius: 30,
+    width: 300,
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
